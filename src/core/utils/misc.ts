f998ea76593a568b0e8546c81b39cdd69ce082dc
keyanmaskoot <keyanmaskoot@gmail.com>
2024-08-23 15:27:01 +0200

    test: minimal changes to test param application

--- src/core/utils/misc.ts
+++ src/core/utils/misc.ts
@@ -1,5 +1,6 @@
 import { Constr, Lucid, SpendingValidator, WithdrawalValidator, applyParamsToScript } from "@anastasia-labs/lucid-cardano-fork";
 import { CborHex, OfferValidators } from "../types.js";
+import { applyParamsToScript as leApply, Constr as LEConstr } from "@lucid-evolution/lucid";
 
 export const getOfferValidators = (
   lucid: Lucid,
@@ -14,9 +15,25 @@ export const getOfferValidators = (
   const stakingCred = new Constr(0, 
     [new Constr(1, [lucid.utils.validatorToScriptHash(stakingVal)])]);
 
+  const lucidCBOR = applyParamsToScript(scripts.spending, [stakingCred]);
+
+  console.log("======================= LUCID =======================");
+  console.log(lucidCBOR);
+  console.log("=====================================================");
+  console.log();
+
+  const leStakingCred = new LEConstr(0, 
+    [new LEConstr(1, [lucid.utils.validatorToScriptHash(stakingVal)])]);
+  const leCBOR = leApply(scripts.spending, [leStakingCred]);
+
+  console.log("=================== LUCID EVOLUTION =================");
+  console.log(leCBOR);
+  console.log("=====================================================");
+
+
   const directOfferVal : SpendingValidator = {
     type: "PlutusV2",
-    script: applyParamsToScript(scripts.spending, [stakingCred]),
+    script: lucidCBOR,
   }
   const directOfferValAddress = lucid.utils.validatorToAddress(directOfferVal);
   
